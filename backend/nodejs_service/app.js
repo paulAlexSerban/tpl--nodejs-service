25a3bf34bce6f0ec2c3b4974e9100c5b7f0ce75f
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-06 18:18:51 +0200

    feat(adust): adjusted scripts - prep for node-farm

--- backend/nodejs_service/app.js
+++ backend/nodejs_service/app.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const routes = require('./routes');
 
+const PORT = 3000;
+
 const server = http.createServer(routes);
 
-server.listen(3000);
+server.listen(PORT, () => {
+  console.log(`Listen to requests on port http://localhost:${PORT}`)
+});
