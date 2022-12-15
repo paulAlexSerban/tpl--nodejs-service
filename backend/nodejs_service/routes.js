ca8763553efc82ec955e08e1d091b8ebaeb07966
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-15 15:02:25 +0200

    feat: added some example node js code

--- backend/nodejs_service/routes.js
+++ backend/nodejs_service/routes.js
@@ -1,29 +1,38 @@
 const fs = require("fs");
+const { mkdir } = require("fs/promises");
+const url = require("url");
 
 const requestHandler = (req, res) => {
-  const url = req.url;
+  const reqUrl = req.url;
   const method = req.method;
+  const parsedUrl = url.parse(reqUrl, true);
+  const query = parsedUrl.query;
+  const path = parsedUrl.path; // returns the full path, including the query
+  const pathname = parsedUrl.pathname;
 
   res.setHeader("Content-Type", "text/html");
 
-  if (url === "/submit-message") {
+  if (pathname === "/submit-message") {
     res.write(`
     <html>
       <head>
         <title>Enter message:</title>
       </head>
       <body>
+        <h1>Template NodeJS - Service</h1>
+        <h2>Submit Message</h2>
         <form action="/message" method="POST">
           <input type="text" name="message">
           <button type="submit">Submit</button>
         </form>
+        ${query ? query.content : "no message"}
       </body>
     </html>
   `);
     return res.end();
   }
 
-  if (url === "/message" && method === "POST") {
+  if (pathname === "/message" && method === "POST") {
     const body = [];
 
     req.on("data", (chunk) => {
@@ -33,12 +42,41 @@ const requestHandler = (req, res) => {
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFile(`message-${Date.now()}.txt`, message, (error) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/submit-message");
-        return res.end();
+
+      fs.mkdir("messages", (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
       });
+
+      const date = Date.now();
+      fs.promises
+        .writeFile(`messages/message-${date}.txt`, message)
+        .then(() => fs.promises.readFile(`messages/message-${date}.txt`, "utf-8"))
+        .then((data) => {
+          console.log(data);
+          res.statusCode = 302;
+          res.setHeader("Location", `/submit-message?content=${data}`);
+          return res.end();
+        })
+        .catch((err) => console.error(err));
     });
+  }
+
+  if (pathname === "/") {
+    res.write(`
+    <html>
+      <head>
+        <title>NodeJS Service</title>
+      </head>
+      <body>
+        <h1>Template NodeJS - Service</h1>
+        <a href="/submit-message">submit message form</a>
+      </body>
+    </html>
+  `);
+    return res.end();
   }
 };
 
--- scripts/docker/all-clean-stop.bash
+++ scripts/docker/all-clean-stop.bash
@@ -3,8 +3,8 @@
 cd "$(dirname "$0")" || exit
 
 echo " 🛑  STOP ALL 🐳  Docker and clean containers"
-docker compose --env-file ../../config.env \
-  --file ../../docker/nodejs_service.dev.docker-compose.yaml --volumes --rmi all
+docker compose --env-file ../../.env \
+  --file ../../docker/nodejs_service.dev.docker-compose.yaml  down --volumes --rmi all
 docker container prune -f
 docker volume prune -f
 docker image prune -af
