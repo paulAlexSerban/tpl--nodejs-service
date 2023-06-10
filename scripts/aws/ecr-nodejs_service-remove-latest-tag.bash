4dd5c9a9f16414de3a504416db2b9fcab2ff11a7
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-10 20:07:59 +0300

    feat(update): scripts

--- scripts/aws/ecr-nodejs_service-remove-latest-tag.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-source ../../.env.dev
-
-echo "Remove latest tag"
-aws ecr batch-delete-image --repository-name $ECR_REPO_NAME --image-ids imageTag=latest
