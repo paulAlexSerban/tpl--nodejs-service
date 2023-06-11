d47fe607ea0ee1d5a4ec290f711d169a2c4bdd94
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-11 12:16:08 +0300

    feat(scripts): added

---
+++ backend/nodejs_service/scripts/docker-build.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+source ../config.env
+
+echo "🟢 BUILD DOCKER 🐳 ${FORMTED_PROJECT_NAME}"
+docker build --file ../Dockerfile ../ -t paulserbandev/nodejs_service --build-arg NODE_ENV=production
+docker image ls
